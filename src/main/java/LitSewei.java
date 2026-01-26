319498896c967e5e3fcd56bdf26ad70165da4939
XiaoGeNintendo <34835642+XiaoGeNekidora@users.noreply.github.com>
2026-01-26 11:27:21 +0800

    Add mark and unmark feature

--- src/main/java/LitSewei.java
+++ src/main/java/LitSewei.java
@@ -27,9 +27,40 @@ public class LitSewei {
         printDividingLine();
         System.out.println("Here is the TODO list:");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.printf("%d. [%s] %s\n", i+1, tasks.get(i).isDone() ? "X" : " ", tasks.get(i).getName());
+            System.out.printf("%d. [%s] %s\n", i + 1, tasks.get(i).isDone() ? "X" : " ", tasks.get(i).getName());
         }
         printDividingLine();
+    }
+
+    private static void markOrUnmark(String input, boolean mark) {
+        try {
+            int taskId = Integer.parseInt(input.split(" ")[1]);
+
+            if (tasks.get(taskId - 1).isDone() == mark) {
+                printDividingLine();
+                System.out.println("Emm... This task is already " + (mark ? "marked" : "unmarked") + "? >_<");
+                printDividingLine();
+                return;
+            }
+
+            tasks.get(taskId - 1).setDone(mark);
+
+            printDividingLine();
+            if (mark) {
+                System.out.println("Marked this as done: " + tasks.get(taskId - 1).getName() + "!! :>");
+            } else {
+                System.out.println("Marked this as not done yet: " + tasks.get(taskId - 1).getName() + "!! :<");
+            }
+            printDividingLine();
+        } catch (NumberFormatException e) {
+            printDividingLine();
+            System.out.println("I cannot understand >_<. Just give me an ID plz~");
+            printDividingLine();
+        } catch (IndexOutOfBoundsException e) {
+            printDividingLine();
+            System.out.println("The ID you gave me seems wrong >_<. Plz check again~");
+            printDividingLine();
+        }
     }
 
     public static void main(String[] args) {
@@ -53,6 +84,10 @@ public class LitSewei {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 printList();
+            } else if (input.startsWith("mark ")) {
+                markOrUnmark(input, true);
+            } else if (input.startsWith("unmark ")) {
+                markOrUnmark(input, false);
             } else {
                 tasks.add(new Task(input));
 
