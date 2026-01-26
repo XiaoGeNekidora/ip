e5b7f977991b2b6f5ef33e779bde49563314a709
XiaoGeNintendo <34835642+XiaoGeNekidora@users.noreply.github.com>
2026-01-26 11:15:08 +0800

    Format code

--- src/main/java/LitSewei.java
+++ src/main/java/LitSewei.java
@@ -6,34 +6,34 @@ public class LitSewei {
     private static Scanner sc = new Scanner(System.in);
     private static ArrayList<String> tasks = new ArrayList<>();
 
-    private static void printDividingLine(){
+    private static void printDividingLine() {
         System.out.println("____________________________________________________________");
     }
 
-    private static void printGreeting(){
+    private static void printGreeting() {
         printDividingLine();
         System.out.println("Hello! I'm Lit Sewei.\n" +
                 "What can I do for you? UwU");
         printDividingLine();
     }
 
-    private static void printGoodbye(){
+    private static void printGoodbye() {
         printDividingLine();
         System.out.println("Bye. See you next time~~~");
         printDividingLine();
     }
 
-    private static void printList(){
+    private static void printList() {
         printDividingLine();
         System.out.println("Here is the TODO list:");
-        for(int i=0;i<tasks.size();i++){
-            System.out.println((i+1) + ". " + tasks.get(i));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
         }
         printDividingLine();
     }
 
     public static void main(String[] args) {
-        String logo= """
+        String logo = """
                 .____    .__  __      _________                    .__\s
                 |    |   |__|/  |_   /   _____/ ______  _  __ ____ |__|
                 |    |   |  \\   __\\  \\_____  \\_/ __ \\ \\/ \\/ // __ \\|  |
@@ -47,17 +47,17 @@ public class LitSewei {
         printGreeting();
 
         // Main loop
-        while(true){
+        while (true) {
             String input = sc.nextLine().trim();
-            if(input.equalsIgnoreCase("bye")){
+            if (input.equalsIgnoreCase("bye")) {
                 break;
-            }else if(input.equalsIgnoreCase("list")){
+            } else if (input.equalsIgnoreCase("list")) {
                 printList();
-            }else{
+            } else {
                 tasks.add(input);
 
                 printDividingLine();
-                System.out.println("I've noted down this task: "+input+"!!!");
+                System.out.println("I've noted down this task: " + input + "!!!");
                 printDividingLine();
             }
         }
